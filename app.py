6dd16f8858a3be653116e08f15639153b636ea22
coralvanda <coralvanda@gmail.com>
2019-11-03 16:15:30 -0500

    Complete desired local behavior (need to add email)

--- app.py
+++ app.py
@@ -6,13 +6,20 @@ Notes :
 October 27, 2019
 """
 
-from datetime import datetime
+import os
+
+from datetime import datetime, timedelta
+from typing import List
 
 import feedparser
 import pandas
+import requests
 
 
-def get_results_from_url(url: str, date_cutoff = None):
+MAILGUN_API_KEY = 'mailgun_api_key'
+
+
+def get_results_from_url(url: str, date_cutoff = None) -> List[dict]:
     CL_Feed = feedparser.parse(url)
 
     entries_list = []
@@ -25,7 +32,7 @@ def get_results_from_url(url: str, date_cutoff = None):
         date_published = pandas.to_datetime(date_published)
 
         if date_cutoff is not None:
-            date_cutoff = pandas.to_datetime(date_cutoff)
+            date_cutoff = pandas.to_datetime(date_cutoff, utc=True)
 
         if date_cutoff is None or date_published > date_cutoff:
             entries_list.append(
@@ -35,27 +42,44 @@ def get_results_from_url(url: str, date_cutoff = None):
                     link=link,
                     published=date_published))
 
-    print(f'Date of most recent run: {datetime.now()}')
     return entries_list
 
 
+def build_url(item_of_interest: str) -> str:
+    return f'http://providence.craigslist.org/search/sss?format=rss&query={item_of_interest}'
+
+
+def send_email(email_body: str):
+    return requests.post(
+        "https://api.mailgun.net/v3/mail.coralvanda.com/messages",
+        auth=("api", os.environ.get(MAILGUN_API_KEY)),
+        data={"from": "CraigsList searchbot <admin@mail.coralvanda.com>",
+              "to": ["angelinadowell@gmail.com", "coralvanda@gmail.com"],
+              "subject": "Latest search results",
+              "text": email_body})
+
+
 if __name__ == '__main__':
-    # TODO
-    #  fix TypeError: Cannot compare tz-naive and tz-aware timestamps between
-    #  dates from feed and dates generated locally
-    #
-    # TODO
-    #  add a way to email myself the new listings
-    #
-    # TODO
-    #  add a way to persist the datetime for each search for reuse on next run
-    #
-    # TODO
-    #  automate everything
+    twelve_hour_delta = timedelta(hours=12)
+    twelve_hours_ago = datetime.now() - twelve_hour_delta
 
-    item_of_interest = 'refrigerator'
-    rss_url = f'http://providence.craigslist.org/search/sss?format=rss&query={item_of_interest}'
-    response = get_results_from_url(url=rss_url, date_cutoff='2019-10-27 19:00:18.356804')
+    response_list = get_results_from_url(
+        url=build_url(item_of_interest='refrigerator'),
+        date_cutoff=twelve_hours_ago)
 
-    for item in response:
-        print(item['link'])
+    response_list.extend(get_results_from_url(
+        url=build_url(item_of_interest='washer'),
+        date_cutoff=twelve_hours_ago))
+
+    response_list.extend(get_results_from_url(
+        url=build_url(item_of_interest='dryer'),
+        date_cutoff=twelve_hours_ago))
+
+    response_list.extend(get_results_from_url(
+        url=build_url(item_of_interest='washer dryer'),
+        date_cutoff=twelve_hours_ago))
+
+    for entry in response_list:
+        print(entry['title'])
+        print(entry['published'])
+        print()
